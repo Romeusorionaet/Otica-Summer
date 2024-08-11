9aae0b5597fb7b8f2fe8783172c3e82d714f148c
romeusorionaet <romeusoares14569@gmail.com>
2024-08-11 11:39:11 -0300

    feat: created the 'second-section'

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { CarrouselGlasses } from '@/components/carrousel/carrousel-glasses'
 import { Header } from '@/components/header'
 import 'keen-slider/keen-slider.min.css'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
       <Header />
 
       <main className="overflow-hidden">
-        <section className="mx-auto flex min-h-[1000px] w-full max-w-[1680px] flex-col bg-gradient-to-t from-blue_1 via-blue_2 to-blue_1 max-md:items-center md:h-[24rem] md:flex-row md:gap-1 md:bg-gradient-to-r">
+        <section
+          id="first-section"
+          className="mx-auto flex min-h-[1000px] w-full max-w-[1680px] flex-col bg-gradient-to-t from-blue_1 via-blue_2 to-blue_1 max-md:items-center md:h-[24rem] md:flex-row md:gap-1 md:bg-gradient-to-r"
+        >
           <section className="h-[900px] min-w-[300px] pt-64">
             <h2 className="mb-10 text-center text-4xl font-bold md:ml-4 md:text-5xl">
               <span className="text-clVRef">Veja</span> o mundo com clareza e
@@ -30,6 +34,41 @@ export default function Home() {
               incomparável
             </h2>
           </section>
+        </section>
+
+        <section
+          id="second-section"
+          className="mx-auto -mt-24 flex w-full max-w-[1680px] flex-col items-center justify-center gap-10 rounded-lg bg-slate-800 p-4 md:mt-44 md:h-[500px] md:flex-row"
+        >
+          <div className="h-full w-96">
+            <Image
+              width={300}
+              height={300}
+              src="/img/people-models/woman-2.png"
+              alt=""
+              className="h-full w-full rounded-lg object-fill"
+            />
+          </div>
+          <div className="w-full space-y-4 text-center text-blue_1 md:w-1/2 md:text-xl">
+            <h2 className="text-3xl font-bold md:text-4xl">
+              Sua <span className="text-clVRef">Visão</span> com Estilo
+            </h2>
+
+            <p>
+              Descontos Exclusivos de até 50% em lentes e armações de alta
+              qualidade.
+            </p>
+
+            <p>
+              Ajudamos você a encontrar o par de óculos perfeito para realçar
+              sua personalidade.
+            </p>
+
+            <p>
+              Não é apenas uma ótica! É um compromisso com o seu conforto visual
+              e bem-estar.
+            </p>
+          </div>
         </section>
       </main>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="fixed z-10 w-full bg-blue_1">
+    <div className="fixed z-20 w-full bg-blue_1">
       <header className="top-0 mx-auto flex w-full max-w-[1680px] items-center justify-between p-8">
         <div className="flex flex-col text-4xl font-bold">
           <span className="text-clVRef">Ótica</span>
