c1a5ca9bb277d3d1ddcebc3784bed811bd88e6c0
romeusorionaet <romeusoares14569@gmail.com>
2024-08-13 14:53:50 -0300

    feat: added logo type / added information in 'metadata'

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,21 @@
 import { ScrollLinkComponent } from './scroll-link-component'
+import Image from 'next/image'
 
 export function Header() {
   return (
     <div className="fixed z-20 w-full bg-clRef_2">
       <header className="top-0 mx-auto flex w-full max-w-[1680px] items-center justify-between p-8">
-        <div className="flex flex-col text-4xl font-bold">
-          <span className="text-clVRef">Ótica</span>
-          <span className="text-clRef_4">Summer</span>
+        <div className="h-32 w-full md:w-44">
+          <Image
+            width={300}
+            height={300}
+            src="/img/logo.png"
+            alt=""
+            className="h-full w-full rounded-lg"
+          />
         </div>
 
-        <nav className="w-[500px] text-right">
+        <nav className="text-right md:w-[500px]">
           <ul className="flex flex-col justify-between gap-2 md:flex-row md:gap-8">
             <li>
               <ScrollLinkComponent
