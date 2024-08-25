3a2d9fe19093a246ab01656f14e7da3bb549966b
romeusorionaet <romeusoares14569@gmail.com>
2024-08-25 15:49:57 -0300

    chore: style: added border bottom in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function Header() {
   return (
-    <div className="fixed z-20 w-full bg-clRef_2">
+    <div className="fixed z-20 w-full border-b border-clVRef/20 bg-clRef_2">
       <header className="top-0 mx-auto flex w-full max-w-[1680px] items-center justify-between p-8">
         <div className="h-32 w-full md:w-44">
           <Image
